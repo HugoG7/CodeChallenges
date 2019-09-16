afd15c52e2b5a33630c848dd4a3f0eb124a62dab
Hugo Garza <hugo.garza@ibm.com>
2019-09-16 14:37:43 -0500

    Adding search algorithms challenges

---
+++ src/main/java/com/hg/challenges/algorithms/searching/BinarySearch.java
@@ -0,0 +1,41 @@
+package com.hg.challenges.algorithms.searching;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 3, 4, 10, 40 }; 
+		
+		int index = search(arr, 0, arr.length - 1, 1);
+		System.out.println("Index Found in => " + index);
+	}
+	
+	static int search(int[] arr, int l, int r, int target) {
+		if(l < r) {
+			int middle = l + (r - l) / 2;
+			
+			if(arr[middle] == target)
+				return middle;
+			
+			if(arr[middle] > target)
+				//LEFT SIDE
+				return search(arr, l, middle - 1, target);
+			
+			//RIGHT SIDE
+			return search(arr, middle + 1, r, target);
+		}
+		
+		return -1;
+	}
+}
+	
+
+//[0|1|2|03|04]
+//[2|3|4|10|40]
+
+//l = 0, r = 4
+//middle = 0 + (4 - 0) / 2 = 2
+
+//l = 3, r = 4
+//middle = 3 + (4 - 3) / 2 = 3
+
+
